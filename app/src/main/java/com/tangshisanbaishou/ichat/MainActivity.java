074a0f738e7e734694bcd16519c547a4c6afe1c6
xiaohuli <waterdoor@foxmail.com>
2019-08-08 10:34:21 +0800

    feat: 去冗余

--- app/src/main/java/com/tangshisanbaishou/ichat/MainActivity.java
+++ app/src/main/java/com/tangshisanbaishou/ichat/MainActivity.java
@@ -30,61 +30,17 @@ public class MainActivity extends Activity implements OnClickListener {
 
     BridgeWebView webView;
 
-    Button button;
-
-    int RESULT_CODE = 0;
-
-    ValueCallback<Uri> mUploadMessage;
-
-    ValueCallback<Uri[]> mUploadMessageArray;
-
-    static class Location {
-        String address;
-    }
-
-    static class User {
-        String name;
-        Location location;
-        String testStr;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Context mcontext = this.getApplicationContext();
 
         webView = (BridgeWebView) findViewById(R.id.webView);
-
-        button = (Button) findViewById(R.id.button);
-
-        button.setOnClickListener(this);
 
         webView.setDefaultHandler(new DefaultHandler());
 
         webView.setWebChromeClient(new WebChromeClient() {
 
-            @SuppressWarnings("unused")
-            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
-                this.openFileChooser(uploadMsg);
-            }
-
-            @SuppressWarnings("unused")
-            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
-                this.openFileChooser(uploadMsg);
-            }
-
-            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
-                mUploadMessage = uploadMsg;
-                pickFile();
-            }
-
-            @Override
-            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-                mUploadMessageArray = filePathCallback;
-                pickFile();
-                return true;
-            }
         });
 
         webView.loadUrl("file:///android_asset/demo.html");
@@ -94,76 +50,16 @@ public class MainActivity extends Activity implements OnClickListener {
             @Override
             public void handler(String data, CallBackFunction function) {
                 Log.i(TAG, "handler = submitFromWeb, data from web = " + data);
-                //Log.i(TAG, "handler = submitFromWeb, data from web = " + new Gson().toJson(data));
-//                if (data.param == '1') {
-//                    badge.setBadgeCount(getApplicationContext(), 2, R.mipmap.ic_launcher);
-//                }
                 badge.setBadgeCount(getApplicationContext(), Integer.parseInt(data), R.mipmap.ic_launcher);
                 function.onCallBack("submitFromWeb exe, response data 中文 from Java");
             }
 
         });
 
-        User user = new User();
-        Location location = new Location();
-        location.address = "SDU";
-        user.location = location;
-        user.name = "大头鬼";
-
-        webView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
-            @Override
-            public void onCallBack(String data) {
-
-            }
-        });
-
-        webView.send("hello");
-        //Context myCon = new Context();
-        //badge.setBadgeCount(getApplicationContext(), 2, R.mipmap.ic_launcher);
-
-    }
-
-    public void pickFile() {
-        Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        chooserIntent.setType("image/*");
-        startActivityForResult(chooserIntent, RESULT_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == RESULT_CODE) {
-            if (null == mUploadMessage && null == mUploadMessageArray){
-                return;
-            }
-            if(null!= mUploadMessage && null == mUploadMessageArray){
-                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-                mUploadMessage.onReceiveValue(result);
-                mUploadMessage = null;
-            }
-
-            if(null == mUploadMessage && null != mUploadMessageArray){
-                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-                mUploadMessageArray.onReceiveValue(new Uri[]{result});
-                mUploadMessageArray = null;
-            }
-
-        }
     }
 
     @Override
     public void onClick(View v) {
-        if (button.equals(v)) {
-            webView.callHandler("functionInJs", "data from Java", new CallBackFunction() {
-
-                @Override
-                public void onCallBack(String data) {
-                    // TODO Auto-generated method stub
-                    Log.i(TAG, "reponse data from js " + data);
-                }
-
-            });
-        }
-
     }
 
 }
